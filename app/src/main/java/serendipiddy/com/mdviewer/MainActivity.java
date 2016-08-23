5fbf8336003f498af5fc4d27dc3b6da3dd19dbd4
iddy <lemon.and.lime@hotmail.com>
2016-08-23 22:27:38 +1200

    added example/text md rendering to mainActivity

--- app/src/main/java/serendipiddy/com/mdviewer/MainActivity.java
+++ app/src/main/java/serendipiddy/com/mdviewer/MainActivity.java
@@ -2,6 +2,10 @@ package serendipiddy.com.mdviewer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+import in.uncod.android.bypass.Bypass;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView text = (TextView) findViewById(R.id.demoText);
+        Bypass bypass = new Bypass(this);
+        String markdownString = "#Markdown!\n This is the demo text for **Mark***down*.\n\n![An image](an_image.png)\n\n[this in an external link](http://github.com/serendipiddy).\n\n [this is an internal link](file.md).\n";
+        CharSequence string = bypass.markdownToSpannable(markdownString);
+        text.setText(string);
+        text.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
---
+++ planning/progress.md
@@ -0,0 +1,13 @@
+Progress of MdViewer
+====================
+
+Milestones
+----------------
+
+* Render markdown text correctly
+* Render images
+* Follow links on filesystem
+* Format with Material theme
+* Capture open-as/intent
+* Display current file in header, but option to hide (like a regular 
+browser)
